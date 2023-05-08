8718d80decb2bf30b6ce1169f20af584872eef90
MaximeSantos <max.sant@outlook.fr>
2023-05-08 12:14:55 +0200

    Add navbar buttons & main logo with a minimum of CSS

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,8 +1,29 @@
+import { Link } from 'react-router-dom';
+import NavbarButton from '../NavbarButton';
 import './style.scss';
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <header className="navbar">
+      <div className="navbar-left">
+        <Link className="navbar-logo" to="/">
+          <img src="src/assets/logos/BZL-logo_black.svg" alt="Logo principal Breizh Littoral" />
+        </Link>
+      </div>
+
+      <div className="navbar-middle" />
+
+      <div className="navbar-right">
+        <NavbarButton className="navbar-button" route="/favoris" icon="src/assets/icons/navbarButton-favoris.svg" />
+        <NavbarButton className="navbar-button" route="/sports" icon="src/assets/icons/navbarButton-sports.svg" />
+        <NavbarButton className="navbar-button" route="/meteo" icon="src/assets/icons/navbarButton-meteo.svg" />
+        <NavbarButton className="navbar-button" route="/conseils" icon="src/assets/icons/navbarButton-conseils.svg" />
+        <button type="button" className="navbar-button-darkmode">
+          <img src="src/assets/icons/navbarButton-darkmode.svg" alt="Bouton mode sombre/clair" />
+        </button>
+      </div>
+
+    </header>
   );
 }
 
--- src/components/NavbarButton/index.jsx
+++ src/components/NavbarButton/index.jsx
@@ -1,15 +1,19 @@
 import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
 
 import './style.scss';
 
-function NavbarButton({ route }) {
+function NavbarButton({ route, icon }) {
   return (
-    <div>NavbarButton</div>
+    <NavLink className="navbar-button" to={route}>
+      <img src={icon} alt={`icone de lien vers la page ${route}`} />
+    </NavLink>
   );
 }
 
 NavbarButton.propTypes = {
   route: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
 };
 
 export default NavbarButton;
