d05b90d4acb2bdad881d2ca73b95f31075a70632
MaximeSantos <max.sant@outlook.fr>
2023-05-15 00:05:23 +0200

    Change homepage picture and description

--- src/components/HomeMain/MainBanner/index.jsx
+++ src/components/HomeMain/MainBanner/index.jsx
@@ -6,10 +6,10 @@ function MainBanner() {
       <div className="banner-left">
         <h2 className="banner-title"> Bienvenue sur Breizh Littoral </h2>
         <p className="banner-description">
-          Que l&#39;on soit adepte de surf, paddle, bodyboard, longboard, wakeboard, kitesurf,
+          Que vous soyez adepte de surf, paddle, bodyboard, longboard, wakeboard, kitesurf,
           planche à voile, longe-côte, chasse sous-marine ou simplement de passage en bord de mer,
-          Breizh Littoral recense les spots de sports nautiques en Bretagne, en apportant des
-          indications sur la localisation géographique et les moyens d&#39;accès, mais aussi sur
+          Breizh Littoral recense les spots de sports nautiques en Bretagne, en apportant
+          des indications sur la localisation géographique et les moyens d&#39;accès, mais aussi sur
           les bonnes pratiques environnementales ou les conditions météos.
         </p>
         <p>
@@ -17,8 +17,7 @@ function MainBanner() {
         </p>
       </div>
       <div className="banner-right">
-        <h2 className="banner-title">Le spot phare du mois !</h2>
-        <img className="banner-picture" src="https://cdn.pixabay.com/photo/2020/06/17/18/27/sea-5310657_960_720.jpg" alt="Spot phare du mois" />
+        <img className="banner-picture" src="https://cdn.pixabay.com/photo/2017/01/07/14/32/kite-surfing-1960536_1280.jpg" alt="Spot phare du mois" />
       </div>
     </div>
   );
